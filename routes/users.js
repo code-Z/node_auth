aab96d7c17fefe67b23cc3afc730824c739d5d7b
Mithilesh Said <s.mit7816@gmail.com>
2015-12-13 16:45:09 +0530

    testing hotel_save

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,24 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/save_hotel', function (req, res, next) {
-  // if (!req.isAuthenticated())
-  //   return res.json({ result: false, message: 'Unauthorized access!', data: null });
-  console.log(req.user);
+router.post('/save_hotel', function (req, res, next) {
+  if (!req.isAuthenticated())
+    return res.json({ result: false, message: 'Unauthorized access!', data: null });
+  var user = req.user;
+  user.hotels.push(req.body.hotel);
+  user.save(function (err, savedRecord) {
+    if (err)
+      return res.json({ result: false, message: 'Error while saving user', data: null });
+    else
+      return res.json({ result: true, message: 'Hotel record saved successfully!', data: savedRecord.hotels });
+  });
+});
+
+router.get('/saved_hotels', function (req, res, next) {
+  if (!req.isAuthenticated())
+    return res.json({ result: false, message: 'Unauthorized access!', data: null });
+  else
+    return res.json({ result: true, message: '', data: req.user.hotels });
 });
 
 module.exports = router;
